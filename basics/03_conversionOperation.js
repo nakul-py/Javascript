bb8964aee897fb7be66eadc41c2ebc2168c73996
Nakul <nakulverma.py@gmail.com>
2025-03-05 18:36:49 +0530

    conversion of datatypes

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false
+
+/*console.log(typeof score)
+console.log(typeof (score))*/
+
+let valueInNumber = Number(score)
+/*console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "apple"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+/*console.log(booleanIsLoggedIn);*/
+
+// 1 => true; 2 => false
+// "" => false
+// "apple" => true
+
+let AnNumber = "22"
+
+let stringNumber = String(AnNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
