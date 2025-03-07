9a9ec2c1765a4904df656984717fe218f5d394aa
verma-nakul <nakulverma.py@gmail.com>
2025-03-07 14:58:51 +0000

    variables , datatypes, conversion operations and comparison.

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are uysing node js not browser
+
+// code readability should be high
+
+let name = "nakul"
+let age = 2
+let isLoogedIn = false
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefinrd =
+// symbol = unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
