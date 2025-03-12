23383a016a1ee4a0baeb0bd1d91b00887c522ab9
Nakul <nakulverma.py@gmail.com>
2025-03-12 21:03:41 +0530

    refactor scope explanations and examples in 02_scopes.js for clarity and conciseness

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,65 +1,10 @@
-// scopes is a way to define the visibility of variables in a program
-// There are 3 types of scopes in JavaScript:
-// 1. Global scope
-// 2. Function scope
-// 3. Block scope
-//
-// Global scope: A variable is in the global scope if it is declared outside of a function or block.
-// Function scope: A variable is in the function scope if it is declared inside a function.
-// Block scope: A variable is in the block scope if it is declared inside a block.
-//
-// The scope of a variable determines where the variable can be accessed in a program.
-//____________________________________________________________________________________________________________________________________________________________________________________
-//
-// Global scope     
-// A variable is in the global scope if it is declared outside of a function or block.
-//
-// Example:
-let myname = "Nakul"
-console.log(myname)
+// scopes are the context in which a variable is declared. They are used to determine the visibility and lifetime of a variable.
 
-// In this example, the variable name is in the global scope because it is declared outside of a function or block.
-//____________________________________________________________________________________________________________________________________________________________________________________
-//
-// Function scope
-
-// A variable is in the function scope if it is declared inside a function.
-//
-// Example:
-function sayMyName() {
-  let name = "Nakul"
-  console.log(name)
-}
-sayMyName()
-//
-// In this example, the variable name is in the function scope because it is declared inside the function sayMyName.
-//____________________________________________________________________________________________________________________________________________________________________________________
-//
-// Block scope
-// A variable is in the block scope if it is declared inside a block.
-//
-// Example:
 if (true) {
-  let name = "Nakul"
-  console.log(name)  
+  let a = 10; // block scope
+  const b = 20; // block scope
+  console.log("number:",a);
 }
 
-// In this example, the variable name is in the block scope because it is declared inside the block { }.
-//____________________________________________________________________________________________________________________________________________________________________________________
-//
-// Shadowing
-// Shadowing occurs when a variable in a local scope has the same name as a variable in an outer scope.
-//
-// Example:
-let name = "Nakul"
-                                                                     
-function sayMyName() {
-  let name = "John"
-  console.log(name)
-
-}
-
-sayMyName()
-//
-// In this example, the variable name in the function sayMyName shadows the variable name in the global scope.
-//___________________________________________________________________________________________________________________________________________________________________________________
+let a = 104; // global scope
+console.log(a); // 104
