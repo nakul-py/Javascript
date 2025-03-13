a465ea5cb9800a9d002ccf3f15023d3bf8dc1933
Nakul <nakulverma.py@gmail.com>
2025-03-13 15:16:03 +0530

    new function implementations in 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,8 +3,47 @@
 if (true) {
   let a = 10; // block scope
   const b = 20; // block scope
-  console.log("number:",a);
+  // console.log("number:",a);
 }
 
 let a = 104; // global scope
-console.log(a); // 104
+// console.log(a); // 104
+
+
+function one () {
+  const username = "nakul";
+
+  function two () {
+    const appname = "myapp";
+    console.log(username);
+  }
+  // console.log(appname);
+  two();
+}
+// one();
+
+if(true) {
+  const username = "apple";
+  if (username === "apple") {
+    const appname = "youtube";
+    // console.log(username + " " + appname);
+  }
+  // console.log(appname);  // ReferenceError: appname is not defined
+}
+// console.log(username);   // ReferenceError: username is not defined
+
+// ++++++++++++++++++++++++++++++++++++++ intresting +++++++++++++++++++++++++++++++++++
+
+function addone (num) {
+  return num + 1;
+}
+
+console.log(addone(10));
+
+// addtwo(10); // ReferenceError: Cannot access 'addtwo' before initialization
+
+const addtwo = function (num) { 
+  return num + 2; 
+} 
+
+console.log(addtwo(10));
