91cb25746cd96a33f5496778f52bde6341c28a90
Nakul <nakulverma.py@gmail.com>
2025-04-05 20:55:07 +0530

    add control flow examples using if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+// if
+
+// < , > , <= , >= , == , === , != , !== 
+
+const isUserLoggedIn = true;
+const temperature = 30;
+
+if (temperature > 25) {
+    console.log("It's hot outside");
+}
+else if (temperature < 15) {
+    console.log("It's cold outside");
+}
+else {
+    console.log("It's a nice day");
+}
