5a407a3ca08828d2177f8180621b81f4d740f0b6
Nakul <nakulverma.py@gmail.com>
2025-04-05 21:08:55 +0530

    refactor control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,15 +2,22 @@
 
 // < , > , <= , >= , == , === , != , !== 
 
-const isUserLoggedIn = true;
-const temperature = 30;
+// const temperature = 30;
 
-if (temperature > 25) {
-    console.log("It's hot outside");
+// if (temperature > 25) {
+//     console.log("It's hot outside");
+// }
+// else if (temperature < 15) {
+//     console.log("It's cold outside");
+// }
+// else {
+//     console.log("It's a nice day");
+// }
+
+const score = 100;
+
+if (score >= 90) {
+    const grade = "A";
+    console.log(`Your grade is ${grade}`);
 }
-else if (temperature < 15) {
-    console.log("It's cold outside");
-}
-else {
-    console.log("It's a nice day");
-}
+// console.log(`Your grade is ${grade}`);
