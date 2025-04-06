bc92a6606b85e4608fcf18cc198ae4181f39d4a8
Nakul <nakulverma.py@gmail.com>
2025-04-06 20:44:23 +0530

    add examples of while and do-while loops in two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+let i = 0;
+
+while (i <= 100) {
+    // console.log(`The number is ${i}`);
+    i = i + 2;
+}
+
+
+let myArray = ['batman', 'superman', 'spiderman'];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 3;
+do {
+    console.log(`The score is ${score}`);
+    score = score + 1;
+} while (score <= 5);
