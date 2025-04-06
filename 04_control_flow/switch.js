66bcd40b6ce3e4e8912a6d71b45e206823282680
Nakul <nakulverma.py@gmail.com>
2025-04-06 09:43:27 +0530

    add switch statement examples for month and day

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,72 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 7;
+// for numbers
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+            
+    default:
+        console.log("Invalid month");
+        break;
+}
+
+const day = "Sunday";
+// for strings
+switch (day) {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+
+    case "Thursday":
+        console.log("Today is Thursday");
+        break;
+
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+
+    case "Saturday":
+        console.log("Today is Saturday");
+        break;
+
+    case "Sunday":
+        console.log("Today is Sunday");
+        break;
+
+    case "Holiday":
+        console.log("Today is Holiday");
+        break;
+
+    default:
+        console.log("Invalid day");
+        break;
+}
