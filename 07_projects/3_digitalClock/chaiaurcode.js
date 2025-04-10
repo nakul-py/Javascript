4af3308d03e25a9398a9296df043596e771cfd67
Nakul <nakulverma.py@gmail.com>
2025-04-10 11:30:56 +0530

    add date and calendar display to digital clock

--- 07_projects/3_digitalClock/chaiaurcode.js
+++ 07_projects/3_digitalClock/chaiaurcode.js
@@ -1,7 +1,30 @@
 const clock = document.getElementById("clock");
-
 setInterval(() => {
   time = new Date();
   clock.innerHTML = time.toLocaleTimeString();
 }
+, 1000);
+const calander = document.getElementById("calander");
+setInterval(() => {
+    time = new Date();
+    calander.innerHTML = time.toLocaleDateString();
+    }
+, 1000);
+const day = document.getElementById("day");
+setInterval(() => {
+    time = new Date();
+    day.innerHTML = time.toLocaleString('default', { weekday: 'long' });
+    }
+, 1000);
+const month = document.getElementById("month");
+setInterval(() => {
+    time = new Date();
+    month.innerHTML = time.toLocaleString('default', { month: 'long' });
+    }
+, 1000);
+const year = document.getElementById("year");
+setInterval(() => {
+    time = new Date();
+    year.innerHTML = time.getFullYear();
+    }
 , 1000);
