4359d95e913624618746ec16789f0242f1253e21
Nakul <nakulverma.py@gmail.com>
2025-04-16 12:18:12 +0530

    add additional promises for error handling and user data resolution

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -10,7 +10,7 @@ promiseOne.then(function() {
     console.log("promiseOne consumed")
 })
 
-const promiseTwo = new Promise(function(resolve){
+const promiseTwo = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log('Async task is complete');
         resolve()
@@ -30,3 +30,93 @@ const promiseThree = new Promise(function(resolve, reject){
 promiseThree.then(function(user){
     console.log(user)
 });
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username: "Nakul", password: "123456"})
+        } else{
+            reject("Error: Something went wrong")
+        }
+    },1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user)
+    return {username: user.username ,password: user.password}
+}).then(({username, password}) => {
+    console.log(username)
+    console.log(password)
+}).catch((error) => {
+    console.log(error)
+}).finally(() => {
+    console.log("Promise is either resolved or rejected")
+})
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "Javascript", password: "1256"})
+        } else{
+            reject("Error: Program is wrong...")
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+   try {
+    const response =  await promiseFive
+    console.log(response.username);
+    console.log(response.password);
+    return response
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const data = await response.json()
+//     console.log(data);
+// }
+
+// getAllUsers()
+
+
+// ----------------------------------------------------------------------------------------------
+
+
+// async function getUsers(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json()
+//         console.log(data);
+//         return data
+//     } catch (error) {
+//         console.log("Er: ",error);
+//     }
+// }
+
+// getUsers()
+
+
+// -------------------------------------------------------------------------------------------------
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    if(!response.ok){
+        throw new Error("Network response was not ok")
+    }
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log("Error: ", error);
+})
