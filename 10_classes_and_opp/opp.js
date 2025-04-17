a3f2526aa6bba065409bcb2895867c5468dc3875
Nakul <nakulverma.py@gmail.com>
2025-04-17 11:15:59 +0530

    add User and Car constructors

--- 10_classes_and_opp/opp.js
+++ 10_classes_and_opp/opp.js
@@ -9,5 +9,33 @@ const user = {
     }
 }
 
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails());
+
+function User(username, loginCount, signedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.signedIn = signedIn;
+
+    this.grettings = function () {
+        console.log(`Hello ${this.username}`);
+    }
+    return this;
+}
+
+const userOne = new User("Nakul", 4, true);
+const userTwo = new User("js", 1, false);
+console.log(userOne.constructor);
+// console.log(userTwo);
+
+const Car = function (name, model, year) {
+    this.name = name;
+    this.model = model;
+    this.year = year;
+}
+
+const carOne = new Car("Koenigsegg", "Agera", 2015);
+const carTwo = new Car("Bugatti", "Chiron", 2018);
+
+console.log(carOne instanceof Car);
+console.log(carOne instanceof Object);
