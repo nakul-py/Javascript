2aacab2b33cfa5e65703f8cc203d3a369cfd273d
Nakul <nakulverma.py@gmail.com>
2025-04-17 13:52:19 +0530

    add increment and getAge methods to createUser prototype

--- 10_classes_and_opp/Object.js
+++ 10_classes_and_opp/Object.js
@@ -11,3 +11,32 @@ function createUser(myname, age){
         this.myname = myname,
         this.age = age
 };
+
+createUser.prototype.increment = function(){
+    this.age++;
+}
+
+createUser.prototype.getAge = function(){
+    console.log(`Age is ${this.age}`);
+}
+const wow = new createUser("aplle", 20)
+const yay = new createUser("banana", 30)
+
+wow.getAge();
+yay.getAge();
+
+
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
